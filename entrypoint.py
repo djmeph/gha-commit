88ac6a58493f64f03707febe6eb3153f29b9e46d
nwitte-rocketloans <normwitte@rocketloans.com>
2022-07-12 17:19:17 -0400

    fix broken config line

--- entrypoint.py
+++ entrypoint.py
@@ -44,7 +44,7 @@ def run():
         add_args.append(files)
     if rebase == 'true':
         debug(git(['pull', '--rebase', '--autostash', 'origin', branch]))
-    debug(git(['config', '--global', '--add safe.directory', '/github/workspace']))
+    debug(git(['config', '--global', '--add', 'safe.directory', '/github/workspace']))
     debug(git(['checkout', '-B', branch]))
     debug(git(add_args))
     debug(git(['commit', '-m', commit_message], retcode=None))
